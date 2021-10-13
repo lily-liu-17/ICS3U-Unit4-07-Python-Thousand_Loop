86832fdce708b2be3e93f13ffb60ae105c747c7e
Lily Liu <lily.liu@mths.ca>
2021-10-13 01:40:21 +0000

    added temp

---
+++ constants1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Liu
+# Created on: Oct 2021
+# This program will print 1000-2000 constants
+
+END = 2000
--- thousand_loop.py
+++ thousand_loop.py
@@ -4,19 +4,21 @@
 # Created on: Oct 2021
 # This program will print 1000-2000
 
+import constants1
+
 
 def main():
     # this program will 1000-2000
     output = " "
-    end = 2000
+
     # process & output
-    for counter in range(1000, end + 1):
+    for counter in range(1000, constants1.END + 1):
         if counter % 5 == 0:
             output = str(counter)
         else:
             output += " " + str(counter)
 
-        if counter % 5 == 4 or counter == end:
+        if counter % 5 == 4 or counter == constants1.END:
             print(output)
 
     print("\nDone.")
